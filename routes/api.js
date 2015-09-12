82f71c265becc6b4ca365c46cf6c7d18065989c1
Claire Nessler <clrnessler@gmail.com>
2015-09-12 09:29:53 -0600

    logout working

--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,9 @@ router.post('/signup', function(req, res, next) {
     if(response.length === 0){
       if(req.body.password === req.body.confirmPass){
         var crypted = bcrypt.hashSync(req.body.password, 8);
-        Users.insert({userFirst: req.body.first, userLast: req.body.last, lists: req.body.lists, email: req.body.email, password: crypted}).then(function (user) {
-          res.json({user: user})
+        Users.insert({userFirst: req.body.first, userLast: req.body.last, streetAddress: req.body.streetAddress, city: req.body.city, state: req.body.state, zipcode: req.body.zipcode, private: false, lists: req.body.lists, email: req.body.email, password: crypted}).then(function (user) {
+          loggedIn = true;
+          res.json({user: user, loggedIn: loggedIn})
         });
       }
     }
@@ -25,7 +26,8 @@ router.post('/login', function (req, res, next) {
   Users.findOne({email: req.body.email}).then(function (foundUser) {
     if(foundUser){
       if(bcrypt.compareSync(req.body.password, foundUser.password)){
-        res.json({user: foundUser})
+        loggedIn = true;
+        res.json({user: foundUser, loggedIn: loggedIn})
       }
     } else {
       res.json({errors: "Invalid username / password"})
@@ -38,5 +40,9 @@ router.post('/insert', function (req, res, next) {
   Users.update({_id: req.body._id}, {$push: {lists: {location: req.body.location, title: req.body.title, description: req.body.description, image: req.body.image}}})
 })
 
+router.post('/profile', function (req, res, next) {
+  console.log(req.body);
+  Users.update({_id: req.body._id}, {$set: {userFirst: req.body.first, userLast: req.body.last, streetAddress: req.body.streetAddress, city: req.body.city, state: req.body.state, zipcode: req.body.zipcode, private: false, lists: req.body.lists, email: req.body.email}})
+})
 
 module.exports = router;
