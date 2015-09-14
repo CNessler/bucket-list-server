81aea0bc55143ec6f7168e3b5c5346369823afa8
Claire Nessler <clrnessler@gmail.com>
2015-09-13 21:28:33 -0600

    pending friendships

--- routes/api.js
+++ routes/api.js
@@ -36,13 +36,27 @@ router.post('/login', function (req, res, next) {
 })
 
 router.post('/insert', function (req, res, next) {
-  console.log(req.body, "THIS IS THE BODY");
   Users.update({_id: req.body._id}, {$push: {lists: {location: req.body.location, title: req.body.title, description: req.body.description, image: req.body.image}}})
 })
 
 router.post('/profile', function (req, res, next) {
-  console.log(req.body);
   Users.update({_id: req.body._id}, {$set: {userFirst: req.body.first, userLast: req.body.last, streetAddress: req.body.streetAddress, city: req.body.city, state: req.body.state, zipcode: req.body.zipcode, private: false, lists: req.body.lists, email: req.body.email}})
 })
 
+router.post('/search', function (req, res, next) {
+  var search = req.body.letters;
+  Users.find({userFirst: {'$regex': search}}).then(function (results) {
+    res.json({foundFriends: results})
+  })
+})
+
+router.get('/friend', function (req, res, next) {
+  Users.findOne({_id: req.query.friend}).then(function (friend) {
+    res.json({foundFriend: friend})
+  })
+})
+
+router.post('/addFriend', function (req, res, next) {
+  Users.update({_id: req.body._id}, {$push: {pendingFriends: req.body.friendId}})
+})
 module.exports = router;
